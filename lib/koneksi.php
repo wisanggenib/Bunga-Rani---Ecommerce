c24bb527b6b2449592e2b452d98c40b9215bd396
Wisanggeni B <bagus.wisanggeni4897@gmail.com>
2019-11-11 21:37:49 +0700

    git add *

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -1,6 +1,6 @@
 <?php
 // isi nama host, username mysql, dan password mysql anda
 $host = mysqli_connect("localhost","root","") or die ("Koneksi Gagal");
-$database = mysqli_select_db($host, "toko_rani") or die ("Database tidak bisa dibuka");
+$database = mysqli_select_db($host, "db_bungarani") or die ("Database tidak bisa dibuka");
 
 ?>

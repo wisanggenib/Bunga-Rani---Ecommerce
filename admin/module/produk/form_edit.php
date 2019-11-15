047ead39bdbc6f1e82b10d828eaadd9057d6b0bd
Wisanggeni B <bagus.wisanggeni4897@gmail.com>
2019-11-15 09:49:45 +0700

    editing form edit, and testing upload edit produk

--- admin/module/produk/form_edit.php
+++ admin/module/produk/form_edit.php
@@ -2,90 +2,112 @@
 if (empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])) {
     echo "<center>Untuk mengakses modul, Anda harus login <br>";
     echo "<a href=../../index.php><b>LOGIN</b></a></center>";
-} else { ?>  
-     
+} else { ?>
+
 <main class="main">
-        <!-- Breadcrumb-->
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item">Home</li>
-          <li class="breadcrumb-item">
+    <!-- Breadcrumb-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item">
             <a href="adminweb.php?module=home">Dashboard</a>
-          </li>
-          <li class="breadcrumb-item active">Produk</li>
-          <!-- Breadcrumb Menu-->
-          <li class="breadcrumb-menu d-md-down-none">
+        </li>
+        <li class="breadcrumb-item active">Produk</li>
+        <!-- Breadcrumb Menu-->
+        <li class="breadcrumb-menu d-md-down-none">
             <div class="btn-group" role="group" aria-label="Button group">
-              <a class="btn" href="#">
-                <i class="icon-speech"></i>
-              </a>
-              <a class="btn" href="./">
-                <i class="icon-graph"></i>  Dashboard</a>
-              <a class="btn" href="#">
-                <i class="icon-settings"></i>  Settings</a>
+                <a class="btn" href="#">
+                    <i class="icon-speech"></i>
+                </a>
+                <a class="btn" href="./">
+                    <i class="icon-graph"></i>  Dashboard</a>
+                <a class="btn" href="#">
+                    <i class="icon-settings"></i>  Settings</a>
             </div>
-          </li>
-        </ol>
-        <div class="container-fluid">
-          <div class="animated fadeIn">
+        </li>
+    </ol>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
             <div class="card">
-                  <div class="card-header">
+                <div class="card-header">
                     <strong>Horizontal</strong> Form</div>
-                  <div class="card-body">
+                <div class="card-body">
                     <?php
                     include "../lib/config.php";
                     include "../lib/koneksi.php";
 
                     $idproduk=$_GET['id_produk'];
-                    $queryEdit= mysqli_query($host, "SELECT * from produk WHERE Id_produk='$idproduk'");
+                    $queryEdit= mysqli_query($host, "SELECT * from produk JOIN kategori on produk.id_kategori = kategori.id_kategori WHERE Id_produk='$idproduk'");
 
                     $hasilQuery=mysqli_fetch_array($queryEdit, MYSQLI_ASSOC);
                     $bunga=$hasilQuery['bunga'];
-                    $warna=$hasilQuery['warna'];
                     $gambar=$hasilQuery['gambar'];
                     $harga=$hasilQuery['harga'];
-
+                    $kategori=$hasilQuery['nama_kategori'];
                     ?>
                     <form class="form-horizontal" action="../admin/module/produk/aksi_edit.php" method="post">
-                      <input type="hidden" name="id_produk" value="<?php echo $idproduk; ?>">
-                      <div class="form-group row">
-                        <label class="col-md-3 col-form-label" for="hf-email">bunga</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="bunga" name="bunga" placeholder="bunga" value="<?php echo $bunga; ?>">
-                          <span class="help-block">Masukkan bunga</span>
+                        <input type="hidden" name="id_produk" value="<?php echo $idproduk; ?>">
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label" for="hf-email">bunga</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" id="bunga" name="bunga" placeholder="bunga"
+                                    value="<?php echo $bunga; ?>">
+                                <span class="help-block">Masukkan bunga</span>
+                            </div>
                         </div>
-                      </div>
-                      <div class="form-group row">
-                        <label class="col-md-3 col-form-label" for="hf-password">warna</label>
-                        <div class="col-md-9">
-                          <input type="warna" class="form-control" id="warna" name="warna" placeholder="warna" value="<?php echo $warna; ?>">
-                          <span class="help-block">Masukkan warna</span>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label" for="hf-email">gambar</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" id="gambar" name="gambar" placeholder="gambar"
+                                    value="<?php echo $gambar; ?>">
+                                <span class="help-block">Masukkan gambar</span>
+                            </div>
                         </div>
-                      </div>
-                      <div class="form-group row">
-                        <label class="col-md-3 col-form-label" for="hf-email">gambar</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="gambar" name="gambar" placeholder="gambar" value="<?php echo $gambar; ?>">
-                          <span class="help-block">Masukkan gambar</span>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label" for="hf-email">harga</label>
+                            <div class="col-md-9">
+                                <input type="text" class="form-control" id="harga" name="harga" placeholder="harga"
+                                    value="<?php echo $harga; ?>">
+                                <span class="help-block">Masukkan harga</span>
+                            </div>
                         </div>
-                      </div>
-                      <div class="form-group row">
-                        <label class="col-md-3 col-form-label" for="hf-email">harga</label>
-                        <div class="col-md-9">
-                          <input type="text" class="form-control" id="harga" name="harga" placeholder="harga" value="<?php echo $harga; ?>">
-                          <span class="help-block">Masukkan harga</span>
+                        <div class="form-group row">
+                            <label class="col-md-3 col-form-label" for="hf-password">Kategori</label>
+                            <div class="col-md-9">
+                                <?php
+                                  $sql= mysqli_query($host, "SELECT * from kategori");
+                                  while($data=mysqli_fetch_array($sql, MYSQLI_ASSOC)){
+                                ?>
+                                <label class="radio-inline" style="padding-left:10px;">
+                                    <?php 
+                                  if($kategori = $data['nama_kategori']){
+                                    ?>
+                                    <input type="radio" name="kategori"
+                                        value="<?php echo $data['id_kategori'] ?> checked"><?php echo $data['nama_kategori'] ?>
+                                    <?php
+                                  }else{
+                                   ?>
+                                    <input type="radio" name="kategori"
+                                        value="<?php echo $data['id_kategori'] ?>"><?php echo $data['nama_kategori'] ?>
+                                    <?php
+                                  }
+                                   ?>
+                                </label>
+                                <?php
+                                  }
+                                ?>
+                            </div>
                         </div>
-                      </div>
-                     
-                    
-                  </div>
-                  <div class="card-footer">
-                    <button class="btn btn-sm btn-primary" type="submit">
-                      <i class="fa fa-dot-circle-o"></i> Submit</button>
-                  </div>
+                        <input type="radio" name="kategori"
+                                        value="<?php echo $data['id_kategori'] ?> checked"><?php echo $data['nama_kategori'] ?>
                 </div>
-                </form>
+                <div class="card-footer">
+                    <button class="btn btn-sm btn-primary" type="submit">
+                        <i class="fa fa-dot-circle-o"></i> Submit</button>
+                </div>
+            </div>
+            </form>
         </div>
-      </main>
-  </aside>
+</main>
+</aside>
 </div>
 <?php } ?>

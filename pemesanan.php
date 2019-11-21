1944755248f964f7350f887c1d4f77c8096c19e4
wisanggenib <bagus.wisanggeni4897@gmail.com>
2019-11-21 14:53:35 +0700

    Fixing folder template and add new folder on pages (Pemesanan)

--- pemesanan.php
+++ pemesanan.php
@@ -4,11 +4,11 @@ session_start();
 if (empty($_SESSION['idpelanggan'])) {
 
 include "template/header.php";
-include "pages/pemesanan.php";
+include "pages/pemesanan/pemesanan.php";
 include "template/footer.php"; 
 }else{
 include "template/header2.php";
-include "pages/pemesanan.php";
+include "pages/pemesanan/pemesanan.php";
 include "template/footer.php"; 
 }
 ?>

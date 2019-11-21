b5d3b75ed9e76d8f78c05d9874301893fe6f5f18
wisanggenib <bagus.wisanggeni4897@gmail.com>
2019-11-21 16:16:15 +0700

    fix query save to pesanan and detail

--- pages/pemesanan/aksi_simpan.php
+++ pages/pemesanan/aksi_simpan.php
@@ -21,9 +21,20 @@ if (empty($_SESSION['idpelanggan']) AND empty($_SESSION['pelanggan'])) {
 
     $querySimpan = mysqli_query($host, "INSERT INTO pesanan (Id_pelanggan,Tgl_pesanan,Tgl_pengiriman,Alamat,Status,Total_bayar,Deskripsi) VALUES ('$id_pelanggan','$tgl_pesan','$tgl_kirim','$alamat','belum','$total_bayar','$deskripsi')");
 
+    $sql = "SELECT * FROM pesanan  WHERE Id_pelanggan = $_SESSION[idpelanggan] and status = 'belum' ORDER BY Id_pesanan DESC LIMIT 1 ";
+    $result = $koneksi->query($sql);
+    $pesanan = $result->fetch_assoc();
+    $id_pesanan_sementara = $pesanan['Id_pesanan'];
+
+    foreach ($_SESSION['cart'] as $item) {
+        $querySimpan = mysqli_query($host, "INSERT INTO detail_pesanan (id_pesanan,quantity,id_produk,harga) VALUES ('$id_pesanan_sementara','$item[quantity]','$item[id]','$item[harga]')");        
+    }
+
+    unset($_SESSION['cart']);
+
 // jika query berhasil maka akan tampil alert dan halaman akan kembali ke daftar layanan
     if ($querySimpan) {
-        echo "<script> alert('Data pesanan Berhasil Masuk'); window.location = '../../cart.php';</script>";
+        echo "<script> alert('Data pesanan Berhasil Masuk'); window.location = '../../detail_pesanan.php';</script>";
 // jika query gagal, akan tampil alert dan halaman akan diarahkan ke form tambah layanan
     } else {
         echo "<script> alert('Data Pesanan Gagal Dimasukkan'); window.location = '../../pemesanan.php';</script>";

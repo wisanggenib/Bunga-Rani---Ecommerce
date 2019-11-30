dc287144ccedd0b72054243aebfd505518d8962e
root <root@pop-os.localdomain>
2019-11-30 18:01:06 +0700

    cekmail.php

--- cekmail.php
+++ cekmail.php
@@ -1,9 +1,11 @@
-<?php
-$to = "somebody@example.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-
-mail($to,$subject,$txt,$headers);
-?> 
+    <?php
+        ini_set( 'display_errors', 1 );
+        error_reporting( E_ALL );
+        $from = "bagus.wisanggeni040897@gmail.com";
+        $to = "bagus.wisanggeni4897@gmail.com";
+        $subject = "Checking PHP mail";
+        $message = "PHP mail works just fine";
+        $headers = "From:" . $from;
+        mail($to,$subject,$message, $headers);
+        echo "The email message was sent.";
+    ?>

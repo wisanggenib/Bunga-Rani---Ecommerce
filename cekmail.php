6d14f03a95f00aa1945a04ffe54a94a04c715ae2
root <root@pop-os.localdomain>
2019-12-02 20:47:37 +0700

    Backup Send mail after konfirmasi pemesanan on cekmail.php

--- cekmail.php
+++ cekmail.php
@@ -1,11 +1,26 @@
-    <?php
-        ini_set( 'display_errors', 1 );
-        error_reporting( E_ALL );
-        $from = "bagus.wisanggeni040897@gmail.com";
-        $to = "bagus.wisanggeni4897@gmail.com";
-        $subject = "Checking PHP mail";
-        $message = "PHP mail works just fine";
-        $headers = "From:" . $from;
-        mail($to,$subject,$message, $headers);
-        echo "The email message was sent.";
-    ?>
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../../MAILER/src/Exception.php';
+require '../../MAILER/src/PHPMailer.php';
+require '../../MAILER/src/SMTP.php';
+
+$nama = 'Eko Maulana';
+$total = '3000';
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+$mail->SMTPDebug = 2; 
+$mail->Host = "mail.bungarani.com "; 
+$mail->Port = "587"; // typically 587 
+$mail->SMTPSecure = 'tls'; // ssl is depracated
+$mail->SMTPAuth = true;
+$mail->Username = "cs@bungarani.com";
+$mail->Password = "Berkah22";
+$mail->setFrom("cs@bungarani.com", "Eko");
+$mail->addAddress("bagus.wisanggeni4897@gmail.com", "Bagus Wisanggeni");
+$mail->Subject = 'Pemesanan';
+$mail->msgHTML("Yang terhormat Bapak/Ibu $nama kami telah menerima pembayaran pesanan anda senilai Rp.$total,00. Silahkan menunggu pesanan diantar<br>TERIMA KASIH<br>Salam Hangan Cs Bunga Rani"); // remove if you do not want to send HTML email
+$mail->AltBody = 'HTML not supported';
+$mail->send();
+?>

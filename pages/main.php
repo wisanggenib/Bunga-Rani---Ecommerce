c667778632004e249c1ecd6452cc0522ea873356
root <root@pop-os.localdomain>
2019-12-02 20:49:43 +0700

    Fix front end for Main.php

--- pages/main.php
+++ pages/main.php
@@ -304,188 +304,14 @@
 
 
 <!-- Banner2 -->
-<section class="banner2 bg5 p-t-55 p-b-55">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto p-t-15 p-b-15">
-                <div class="hov-img-zoom pos-relative">
-                    <img src="asset/images/banner-08.jpg" alt="IMG-BANNER">
 
-                    <div class="ab-t-l sizefull flex-col-c-m p-l-15 p-r-15">
-                        <span class="m-text9 p-t-45 fs-20-sm">
-                            The Beauty
-                        </span>
-
-                        <h3 class="l-text1 fs-35-sm">
-                            Lookbook
-                        </h3>
-
-                        <a href="#" class="s-text4 hov2 p-t-20 ">
-                            View Collection
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-10 col-md-8 col-lg-6 m-l-r-auto p-t-15 p-b-15">
-                <div class="bgwhite hov-img-zoom pos-relative p-b-20per-ssm">
-                    <img src="asset/images/shop-item-09.jpg" alt="IMG-BANNER">
-
-                    <div class="ab-t-l sizefull flex-col-c-b p-l-15 p-r-15 p-b-20">
-                        <div class="t-center">
-                            <a href="product-detail.html" class="dis-block s-text3 p-b-5">
-                                Gafas sol Hawkers one
-                            </a>
-
-                            <span class="block2-oldprice m-text7 p-r-5">
-                                $29.50
-                            </span>
-
-                            <span class="block2-newprice m-text8">
-                                $15.90
-                            </span>
-                        </div>
-
-                        <div class="flex-c-m p-t-44 p-t-30-xl">
-                            <div class="flex-col-c-m size3 bo1 m-l-5 m-r-5">
-                                <span class="m-text10 p-b-1 days">
-                                    69
-                                </span>
-
-                                <span class="s-text5">
-                                    days
-                                </span>
-                            </div>
-
-                            <div class="flex-col-c-m size3 bo1 m-l-5 m-r-5">
-                                <span class="m-text10 p-b-1 hours">
-                                    04
-                                </span>
-
-                                <span class="s-text5">
-                                    hrs
-                                </span>
-                            </div>
-
-                            <div class="flex-col-c-m size3 bo1 m-l-5 m-r-5">
-                                <span class="m-text10 p-b-1 minutes">
-                                    32
-                                </span>
-
-                                <span class="s-text5">
-                                    mins
-                                </span>
-                            </div>
-
-                            <div class="flex-col-c-m size3 bo1 m-l-5 m-r-5">
-                                <span class="m-text10 p-b-1 seconds">
-                                    05
-                                </span>
-
-                                <span class="s-text5">
-                                    secs
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 
 
 <!-- Blog -->
-<section class="blog bgwhite p-t-94 p-b-65">
-    <div class="container">
-        <div class="sec-title p-b-52">
-            <h3 class="m-text5 t-center">
-                Our Blog
-            </h3>
-        </div>
-
-        <div class="row">
-            <div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
-                <!-- Block3 -->
-                <div class="block3">
-                    <a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
-                        <img src="asset/images/blog-01.jpg" alt="IMG-BLOG">
-                    </a>
-
-                    <div class="block3-txt p-t-14">
-                        <h4 class="p-b-7">
-                            <a href="blog-detail.html" class="m-text11">
-                                Black Friday Guide: Best Sales & Discount Codes
-                            </a>
-                        </h4>
-
-                        <span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
-                        <span class="s-text6">on</span> <span class="s-text7">July 22, 2017</span>
-
-                        <p class="s-text8 p-t-16">
-                            Duis ut velit gravida nibh bibendum commodo. Sus-pendisse pellentesque mattis augue id
-                            euismod. Inter-dum et malesuada fames
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
-                <!-- Block3 -->
-                <div class="block3">
-                    <a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
-                        <img src="asset/images/blog-02.jpg" alt="IMG-BLOG">
-                    </a>
-
-                    <div class="block3-txt p-t-14">
-                        <h4 class="p-b-7">
-                            <a href="blog-detail.html" class="m-text11">
-                                The White Sneakers Nearly Every Fashion Girls Own
-                            </a>
-                        </h4>
-
-                        <span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
-                        <span class="s-text6">on</span> <span class="s-text7">July 18, 2017</span>
-
-                        <p class="s-text8 p-t-16">
-                            Nullam scelerisque, lacus sed consequat laoreet, dui enim iaculis leo, eu viverra ex nulla
-                            in tellus. Nullam nec ornare tellus, ac fringilla lacus. Ut sit ame
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-sm-10 col-md-4 p-b-30 m-l-r-auto">
-                <!-- Block3 -->
-                <div class="block3">
-                    <a href="blog-detail.html" class="block3-img dis-block hov-img-zoom">
-                        <img src="asset/images/blog-03.jpg" alt="IMG-BLOG">
-                    </a>
-
-                    <div class="block3-txt p-t-14">
-                        <h4 class="p-b-7">
-                            <a href="blog-detail.html" class="m-text11">
-                                New York SS 2018 Street Style: Annina Mislin
-                            </a>
-                        </h4>
-
-                        <span class="s-text6">By</span> <span class="s-text7">Nancy Ward</span>
-                        <span class="s-text6">on</span> <span class="s-text7">July 2, 2017</span>
-
-                        <p class="s-text8 p-t-16">
-                            Proin nec vehicula lorem, a efficitur ex. Nam vehicula nulla vel erat tincidunt, sed
-                            hendrerit ligula porttitor. Fusce sit amet maximus nunc
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 
 
 <!-- Shipping -->
-<section class="shipping bgwhite p-t-62 p-b-46">
+<!-- <section class="shipping bgwhite p-t-62 p-b-46">
     <div class="flex-w p-l-15 p-r-15">
         <div class="flex-col-c w-size5 p-l-15 p-r-15 p-t-16 p-b-15 respon1">
             <h4 class="m-text12 t-center">
@@ -517,4 +343,4 @@
             </span>
         </div>
     </div>
-</section>
+</section> -->

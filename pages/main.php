fe9ff6710f01301d664996f31c60ff019bb6c547
root <root@pop-os.localdomain>
2019-12-07 16:48:16 +0700

    slider on index for user

--- pages/main.php
+++ pages/main.php
@@ -1,48 +1,33 @@
-<!-- Slide1 -->
+
 <section class="slide1">
     <div class="wrap-slick1">
         <div class="slick1">
-            <div class="item-slick1 item1-slick1" style="background-image: url('asset/images/bg-banner-01.png');">
+            <?php 
+                $sql = "SELECT * FROM slider ORDER BY id_slider DESC";
+                    $result = $koneksi->query($sql);
+                    while($slider = $result->fetch_assoc()) {
+             ?>
+            <div class="item-slick1 item1-slick1" style="background-image: url('asset/images/slider/<?=$slider['gambar']?>');">
                 <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
                     <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15"
                     data-appear="fadeInDown">
-                    Women Collection 2018
+                    <?=$slider['pesan']?>
                 </span>
 
                 <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-                    New arrivals
+                    <?=$slider['pesan1']?>
                 </h2>
 
                 <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
 
-                    <a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
+                    <a href="produk.php" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
                         Shop Now
                     </a>
                 </div>
             </div>
         </div>
-
-        <div class="item-slick1 item2-slick1" style="background-image: url(asset/images/master-slide-02.png);">
-            <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
-                <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="rollIn">
-                    Women Collection 2018
-                </span>
-
-                <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37"
-                data-appear="lightSpeedIn">
-                New arrivals
-            </h2>
-
-            <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="slideInUp">
-
-                <a href="product.html" class="flex-c-m size2 bo-rad-23 s-text2 bgwhite hov1 trans-0-4">
-                    Shop Now
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="item-slick1 item3-slick1" style="background-image: url(asset/images/master-slide-03.png);">
+    <?php } ?>
+<!--     <div class="item-slick1 item1-slick1" style="background-image: url(asset/images/master-slide-03.png);">
         <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
             <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15"
             data-appear="rotateInDownLeft">
@@ -61,7 +46,7 @@
         </a>
     </div>
 </div>
-</div>
+</div> -->
 
 </div>
 </div>
@@ -343,4 +328,4 @@
             </span>
         </div>
     </div>
-</section> -->
+</section>

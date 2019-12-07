45226821f58ba5ff6420e621849c8b3fd615321f
root <root@pop-os.localdomain>
2019-12-07 15:23:32 +0700

    Beautify Admin

--- admin/module/pesanan/list_pesanan.php
+++ admin/module/pesanan/list_pesanan.php
@@ -1,24 +1,6 @@
 <main class="main">
   <!-- Breadcrumb-->
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item">Home</li>
-    <li class="breadcrumb-item">
-      <a href="#">Pesanan</a>
-    </li>
-    <li class="breadcrumb-item active">Dashboard</li>
-    <!-- Breadcrumb Menu-->
-    <li class="breadcrumb-menu d-md-down-none">
-      <div class="btn-group" role="group" aria-label="Button group">
-        <a class="btn" href="#">
-          <i class="icon-speech"></i>
-        </a>
-        <a class="btn" href="./">
-          <i class="icon-graph"></i>  Dashboard</a>
-          <a class="btn" href="#">
-            <i class="icon-settings"></i>  Settings</a>
-          </div>
-        </li>
-      </ol>
+  <ol class="breadcrumb"></ol>
       <div class="container-fluid">
         <div class="animated fadeIn">
           <div class="row">

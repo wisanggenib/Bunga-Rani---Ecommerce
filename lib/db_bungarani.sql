828c6eacdb64413fbe49e24af256970d33f55abc
root <root@pop-os.localdomain>
2019-12-15 08:08:39 +0700

    Updating DB

--- lib/db_bungarani.sql
+++ lib/db_bungarani.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 24, 2019 at 01:13 PM
+-- Generation Time: Dec 15, 2019 at 02:06 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -71,7 +71,10 @@ INSERT INTO `detail_pesanan` (`id_detail_pesanan`, `id_pesanan`, `quantity`, `id
 (6, 55, 3, 23, 123),
 (7, 56, 1, 25, 30000),
 (8, 57, 4, 25, 30000),
-(9, 57, 1, 22, 50000);
+(9, 57, 1, 22, 50000),
+(10, 58, 3, 25, 30000),
+(11, 58, 2, 23, 123),
+(12, 59, 1, 26, 10000);
 
 -- --------------------------------------------------------
 
@@ -108,17 +111,19 @@ CREATE TABLE `pelanggan` (
   `nama_belakang` varchar(20) NOT NULL,
   `alamat` varchar(15) NOT NULL,
   `no_hp` int(12) NOT NULL,
-  `email` varchar(30) NOT NULL
+  `email` varchar(30) NOT NULL,
+  `gambar` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `pelanggan`
 --
 
-INSERT INTO `pelanggan` (`Id_pelanggan`, `username`, `password`, `nama_depan`, `nama_belakang`, `alamat`, `no_hp`, `email`) VALUES
-(1, 'eko', 'eko', 'eko', '', 'sini', 2147483647, 'eko@eko.com'),
-(8, 'asd', 'asd', 'asd', '', 'asd', 123, 'asd@hmail.com'),
-(9, 'gus', 'gus', 'gushus', '', 'gus', 123123, 'gus@gmail.com');
+INSERT INTO `pelanggan` (`Id_pelanggan`, `username`, `password`, `nama_depan`, `nama_belakang`, `alamat`, `no_hp`, `email`, `gambar`) VALUES
+(1, 'eko', 'eko', 'eko2222', '1', 'sini1', 11, 'eko1@eko.com', 0x6c6f676f5f616d696b6f6d5f66756c6c5f636f6c6f722e706e67),
+(9, 'gus', 'gus', 'gushus', '', 'gus', 123123, 'gus@gmail.com', ''),
+(24, 'apin', 'apin', 'apin', 'apin', 'sulawesi', 876661, 'asropinr@gmail.com', 0x696e6465782e6a706567),
+(25, 'bagusss', 'bagus', 'bagus', 'bagus', 'bagus', 123, 'bagus@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
@@ -143,7 +148,9 @@ INSERT INTO `pembayaran` (`Id_pembayaran`, `Tgl_pembayaran`, `Gambar`, `Id_pesan
 (14, '2019-11-24', 0x313832303331353338382e6a7067, 53, 'sudah', '123123'),
 (15, '2019-11-21', 0x383437373131365f525f5a303031412e6a7067, 55, 'sudah', '123'),
 (16, '2019-11-24', 0x313832303331353338382e6a7067, 52, 'sudah', '231312'),
-(17, '2019-11-24', 0x444f754253794455454149714b4f4d2e6a7067, 57, 'sudah', '12312312312321');
+(17, '2019-11-24', 0x444f754253794455454149714b4f4d2e6a7067, 57, 'sudah', '12312312312321'),
+(19, '2019-11-29', 0x6c6f676f5f616d696b6f6d5f66756c6c5f636f6c6f722e706e67, 58, 'pending', NULL),
+(20, '2019-12-02', 0x6c6f676f5f616d696b6f6d5f66756c6c5f636f6c6f722e706e67, 59, 'pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -172,7 +179,9 @@ INSERT INTO `pesanan` (`Id_pesanan`, `Id_pelanggan`, `Tgl_pesanan`, `Tgl_pengiri
 (53, 1, '2019-11-21', '2019-11-22', 'aa', 'sudah', 100000, 'a'),
 (55, 1, '2019-11-21', '2019-11-30', 'Jember', 'sudah', 369, 'Kontak saya 097'),
 (56, 1, '2019-11-24', '2019-11-23', 'xcc', 'belum', 30000, 'asd'),
-(57, 1, '2019-11-24', '2019-11-29', 'Jl Tluki 1 No 105 Condong Catur Depok Sleman Yogyakarta', 'sudah', 170000, 'terima kasih');
+(57, 1, '2019-11-24', '2019-11-29', 'Jl Tluki 1 No 105 Condong Catur Depok Sleman Yogyakarta', 'sudah', 170000, 'terima kasih'),
+(58, 1, '2019-11-29', '2019-11-22', 'asd', 'pending', 90246, 'asd'),
+(59, 9, '2019-12-02', '2019-12-03', '123', 'pending', 10000, '123');
 
 -- --------------------------------------------------------
 
@@ -195,7 +204,30 @@ CREATE TABLE `produk` (
 INSERT INTO `produk` (`Id_produk`, `nama_produk`, `gambar`, `harga`, `id_kategori`) VALUES
 (22, 'Mawar Putih 2', '1820315388.jpg', 50000, 2),
 (23, 'Mawar Merah', 'ee64d028c4add3e0ab16a997ebf449fc.jpg', 123, 1),
-(25, 'produk baru', 'Flora-Bunga-Mawar-Simbol-Kasih-Sayang-yang-Melegenda.jpg', 30000, 2);
+(25, 'produk baru', 'Flora-Bunga-Mawar-Simbol-Kasih-Sayang-yang-Melegenda.jpg', 30000, 2),
+(26, 'Testing Produk', 'index.jpeg', 10000, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE `slider` (
+  `id_slider` int(11) NOT NULL,
+  `gambar` blob NOT NULL,
+  `pesan` varchar(10) NOT NULL,
+  `pesan1` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`id_slider`, `gambar`, `pesan`, `pesan1`) VALUES
+(2, 0x62672d62616e6e65722d30332e706e67, 'matahari', 'matahari1'),
+(3, 0x62672d62616e6e65722d30322e706e67, 'Bunga', 'Bunga1'),
+(4, 0x62672d62616e6e65722d30312e706e67, 'Bunga2', 'Bunga2');
 
 --
 -- Indexes for dumped tables
@@ -248,6 +280,12 @@ ALTER TABLE `produk`
   ADD KEY `id_kategori` (`id_kategori`);
 
 --
+-- Indexes for table `slider`
+--
+ALTER TABLE `slider`
+  ADD PRIMARY KEY (`id_slider`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -261,7 +299,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `detail_pesanan`
 --
 ALTER TABLE `detail_pesanan`
-  MODIFY `id_detail_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_detail_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -273,25 +311,31 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `Id_pelanggan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `Id_pelanggan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `Id_pembayaran` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `Id_pembayaran` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `Id_pesanan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `Id_pesanan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `Id_produk` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `Id_produk` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `slider`
+--
+ALTER TABLE `slider`
+  MODIFY `id_slider` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
